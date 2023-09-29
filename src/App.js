68e9802eeda214500982cb9cb224085dc7adc59b
Samaksh Agarwal <agarwalsamaksh11@gmail.com>
2023-09-29 14:54:11 +0530

    Added gallery section and improved image handling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React,{lazy,Suspense, useEffect,useRef,useState} from 'react';
+import React,{lazy,Suspense,useRef,useState} from 'react';
 import './App.css';
 import Loader from './components/loader';
-import MonoActPic from '../src/assets/images/eventMonoact.JPG';
-import NukkadPic from '../src/assets/images/eventNukkad.JPG';
-import StageActPic from '../src/assets/images/eventStage.JPG';
-import MimesPic from '../src/assets/images/eventMimes.jpg';
+import Images from './assets/images';
 import { BrowserRouter as Router, Route , Routes } from 'react-router-dom';
-const BgBlob = lazy(()=> import('./components/bgBlur'));
+const BgBlob = lazy(()=>import('./components/bgBlur'));
 const Cards = lazy(()=>import('./components/cards'));
 const EventsHead = lazy(()=>import('./components/sectionHead'));
 const LandingPage = lazy(()=>import('./components/websiteHeader'));
 const About = lazy(()=>import('./components/about'));
+const Gallery = lazy(()=>import('./components/gallery'));
 
 function App() {
-  const [eventInfo,setEventInfo] = React.useState([
-    {dataImage : `${MonoActPic}` , header : 'Mono Act', description : 'In a monoact, it\'s just you - acting, reacting, and unraveling a scene through a solo monologue. Imagine the intensity of being the sole storyteller, where every emotion, every word is magnified. Captivate the audience with your presence as you embody characters, thoughts, and feelings.' , id : 1},
-    {dataImage : `${NukkadPic}` , header : 'Street Act', description : 'Imagine the deepest messages delivered through simple, raw acting. That\'s Nukkad - where the streets become our stage. Join us in this unique form of theater, where powerful stories meet outdoor spaces, creating unforgettable experiences for all.' , id : 2},
-    {dataImage : `${StageActPic}` , header : 'Stage Act', description : 'Unlike longer plays with multiple acts, a one-act play is a concentrated burst of drama that packs a punch in a single breath. Dive into a world where every word, every moment matters - where storytelling is distilled to its purest form.' , id : 3},
-    {dataImage : `${MimesPic}` , header : 'Mimes', description : 'Silent tales sculpted by movement, Mimes are poetry without words. Emotions dance, stories unfold, all in the elegant language of gestures. Watch as invisible walls and tangible joys come alive, speaking volumes in silence. A mesmerizing art that transcends language, Mime paints stories on the canvas of imagination.' , id : 4}
+  const [eventInfo,setEventInfo] = useState([
+    {dataImage : `${Images.MonoActPic}` , header : 'Mono Act', description : 'In a monoact, it\'s just you - acting, reacting, and unraveling a scene through a solo monologue. Imagine the intensity of being the sole storyteller, where every emotion, every word is magnified. Captivate the audience with your presence as you embody characters, thoughts, and feelings.' , id : 1},
+    {dataImage : `${Images.NukkadPic}` , header : 'Street Act', description : 'Imagine the deepest messages delivered through simple, raw acting. That\'s Nukkad - where the streets become our stage. Join us in this unique form of theater, where powerful stories meet outdoor spaces, creating unforgettable experiences for all.' , id : 2},
+    {dataImage : `${Images.StageActPic}` , header : 'Stage Act', description : 'Unlike longer plays with multiple acts, a one-act play is a concentrated burst of drama that packs a punch in a single breath. Dive into a world where every word, every moment matters - where storytelling is distilled to its purest form.' , id : 3},
+    {dataImage : `${Images.MimesPic}` , header : 'Mimes', description : 'Silent tales sculpted by movement, Mimes are poetry without words. Emotions dance, stories unfold, all in the elegant language of gestures. Watch as invisible walls and tangible joys come alive, speaking volumes in silence. A mesmerizing art that transcends language, Mime paints stories on the canvas of imagination.' , id : 4}
   ])
 
   return (
@@ -29,8 +27,9 @@ function App() {
               element={<Suspense fallback={<Loader/>}><LandingPage/>
               <About/>
               <EventsHead/>
-              <Cards eventInfo={eventInfo}/>
-              <BgBlob/></Suspense>}
+              <Cards eventInfo={eventInfo} />
+              <BgBlob/>
+              <Gallery/></Suspense>}
             />
           </Routes>
         </Router>
---
+++ src/components/gallery/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Images from '../../assets/images'
+
+const Gallery = () => {
+  return (
+    <div className='gallery'>
+        <div className="gallery_header">
+            <h1>GALLERY</h1>
+        </div>
+        <div className="gallery_media">
+            <img src={Images.img1} alt="MonoActPic"/>
+            <img src={Images.img2} alt="NukkadPic"/>
+            <img src={Images.img3} alt="StageActPic"/>
+            <img src={Images.img4} alt="MimesPic"/>
+            <img src={Images.img1} alt="MonoActPic"/>
+            <img src={Images.img2} alt="NukkadPic"/>
+            <img src={Images.img3} alt="StageActPic"/>
+            <img src={Images.img4} alt="MimesPic"/>
+            <img src={Images.img1} alt="MimesPic"/>
+        </div>
+        <a href='/' className="gallery_button"><span>Explore More</span><span>Explore More</span></a>
+    </div>
+  )
+}
+
+export default Gallery
